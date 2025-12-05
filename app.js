43a92d302376374f7daef4f4c4264c7bb8079439
jaspreetsingh36-ops <jaspreet.singh36@ontariotechu.net>
2025-12-05 02:10:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // routes/players.js
 const express = require('express');
 const router = express.Router();
-const Player = require('./models/Players');
+const Player = require('./models/player');
 const { requireAuth } = require('../config/auth');
 
 // ===== PUBLIC ROUTES =====
